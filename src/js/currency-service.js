171b6ae5f0b745b85c64896be2b13e01d78e2a7f
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-12 13:18:58 -0800

    start to implement sessionStorage

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,28 +1,41 @@
 export default class CurrencyService {
   static async getCurrency(currency1, currency2, amount) {
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw Error(response.statusText);
+    const sessionData = sessionStorage.getItem('savedData')
+    if (!savedData) {
+      const savedData = "savedcurrencykey";
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
+      try {
+        const response = await fetch(url);
+        sessionStorage.setItem('savedData', JSON.stringify(response))
+        console.log(savedData + "this is savedData");
+        console.log(sessionData + "this is sessiondata")
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      } catch (error) {
+        document.getElementById("output").innerHTML = Error(error.message);
+        return Error(error.message);
+      }
+    } else {
+      console.log('no api hit')
+      return JSON.parse(sessionData)
     }
-    return response.json();
-  } catch (error) {
-    return Error(error.message);
+
   }
-}
 
 
-static async getList() {
-   const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
-try {
-  const objList = await fetch(url);
-  if (!objList.ok) {
-    throw Error(objList.statusText);
+  static async getList() {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+    try {
+      const objList = await fetch(url);
+      if (!objList.ok) {
+        throw Error(objList.statusText);
+      }
+      return objList.json();
+    } catch (error) {
+      document.getElementById("output").innerHTML = Error(error.message);
+      return Error(error.message);
+    }
   }
-  return objList.json();
-} catch (error) {
-  return Error(error.message);
-}
-}
 }
