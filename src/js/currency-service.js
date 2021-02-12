b0966bce2b16e2737d15fa4790c9029a474aca0f
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-12 10:56:38 -0800

    add second async function and start to implement functionality

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
-  static async getCurrency() {
-  const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+  static async getCurrency(currency1, currency2, amount) {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -10,5 +10,19 @@ export default class CurrencyService {
   } catch (error) {
     return Error(error.message);
   }
+}
+
+
+static async getList() {
+   const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+try {
+  const objList = await fetch(url);
+  if (!objList.ok) {
+    throw Error(objList.statusText);
+  }
+  return objList.json();
+} catch (error) {
+  return Error(error.message);
+}
 }
 }
--- src/main.js
+++ src/main.js
@@ -3,25 +3,28 @@ import $ from 'jquery';
 import './css/styles.css';
 
 async function getCall() {
-  const response = await CurrencyService.getCurrency();
-  
-    console.log(response.statusText)
-    console.log(response)
-    const objKeys = Object.keys(response.conversion_rates)
-    const objValues = Object.keys(response.conversion_rates)
+  let currency1 = document.getElementById("currency1").value;
+  let currency2 = document.getElementById("currency2").value;
+  let amount = document.getElementById("amount").value;
+  const response = await CurrencyService.getCurrency(currency1, currency2, amount);
+  const objList = await CurrencyService.getList();
+  const objKeys = Object.keys(objList.conversion_rates)
+  const objValues = Object.keys(objList.conversion_rates)
 
-    for (let i = 0; i < Object.keys(response.conversion_rates).length; i++) {
-      document.getElementById("currency").innerHTML = document.getElementById("currency").innerHTML + `<option value="${objKeys[i]}"></option>`
-      document.getElementById("currency2").innerHTML = document.getElementById("currency2").innerHTML + `<option value="${objKeys[i]}"></option>`
- 
-    }
+  for (let i = 0; i < Object.keys(objList.conversion_rates).length; i++) {
+    document.getElementById("currency").innerHTML = document.getElementById("currency").innerHTML + `<option value="${objKeys[i]}"></option>`
+    document.getElementById("currency2").innerHTML = document.getElementById("currency2").innerHTML + `<option value="${objKeys[i]}"></option>`
+
+  }
+  document.getElementById("output").innerHTML = `
+  Converting from ${currency1} to ${currency2} . . . 
+  The conversion rate is ${response.conversion_rate}. $${amount}${currency1} is equal to ${response.conversion_result}${currency2}.
+  `
 }
 
 
-$(document).ready(function() { 
-  $('#convert').click(function(event) {
-    const currency1 = document.getElementById("currency1").value;
-    const currency2 = document.getElementById("currency2").value;
+$(document).ready(function () {
+  $('#convert').click(function (event) {
     getCall();
   })
 });
