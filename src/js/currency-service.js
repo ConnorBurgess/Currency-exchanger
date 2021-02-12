c8c035a35c97c9cab9ca40edd728de0dc2e67536
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-12 13:43:35 -0800

    implement sessionStorage on async function getList()

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,41 +1,40 @@
 export default class CurrencyService {
   static async getCurrency(currency1, currency2, amount) {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      document.getElementById("output").innerHTML = Error(error.message);
+      return Error(error.message);
+    }
+  }
+
+  static async getList() {
     const sessionData = sessionStorage.getItem('savedData')
-    if (!savedData) {
-      const savedData = "savedcurrencykey";
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`
+    const key = "exchangeKey";
+    //Check if data in sessionStorage
+    let objList = sessionStorage.getItem(key);
+    if (objList) {
+      console.log("No API hit")
+      return objList.json();
+    }
+    else {
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
       try {
-        const response = await fetch(url);
-        sessionStorage.setItem('savedData', JSON.stringify(response))
-        console.log(savedData + "this is savedData");
-        console.log(sessionData + "this is sessiondata")
-        if (!response.ok) {
-          throw Error(response.statusText);
+        console.log("API hit");
+        objList = await fetch(url);
+        if (!objList.ok) {
+          throw Error(objList.statusText);
         }
-        return response.json();
+        return objList.json();
       } catch (error) {
         document.getElementById("output").innerHTML = Error(error.message);
         return Error(error.message);
       }
-    } else {
-      console.log('no api hit')
-      return JSON.parse(sessionData)
-    }
-
-  }
-
-
-  static async getList() {
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
-    try {
-      const objList = await fetch(url);
-      if (!objList.ok) {
-        throw Error(objList.statusText);
-      }
-      return objList.json();
-    } catch (error) {
-      document.getElementById("output").innerHTML = Error(error.message);
-      return Error(error.message);
     }
   }
 }
